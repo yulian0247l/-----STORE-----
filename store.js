dd190ddf30598770f1b92cde9733bbde9b797052
yulian0247l <yuliansanchez721@gmail.com>
2025-08-11 19:58:36 -0400

    store.js

--- store.js
+++ store.js
@@ -21,31 +21,31 @@ const products = [
     description: "Sistema de arrestar con carcel y trabajo para disminuir el tiempo con acl de policia.",
     price: "$5 USD",
     mediaType: "image",
-    mediaSrc: "https://drive.google.com/uc?export=view&id=1MJNZUnAiejBCI02yLj6HaXJgkLj3qVY3"
+    mediaSrc: "arrestar.jpg"
   },
   {
     id: 4,
     title: "Script de laser",
     description: "Funciona para poder utilizar lazer en las armas.",
     price: "$4 USD",
-    mediaType: "video",
-    mediaSrc: "https://drive.google.com/uc?export=view&id=1sIrnLXFzxU9NIhKB8DpUY0VU1wVIP46-"
+    mediaType: "image",
+    mediaSrc: "laser.jpg"
   },
   {
     id: 5,
     title: "Trabajo de cortador de cesped",
     description: "Trabajo.",
     price: "$1 USD",
-    mediaType: "video",
-    mediaSrc: "https://drive.google.com/uc?export=view&id=15gvSvOP_xwBJQgHixlf8z-7cGM7mapj8"
+    mediaType: "image",
+    mediaSrc: "cesped.jpg"
   },
   {
     id: 6,
     title: "Demo en Video",
-    description: "Video mostrando las funciones del script",
+    description: "foto mostrando las funciones del script",
     price: "$18 USD",
-    mediaType: "video",
-    mediaSrc: "https://drive.google.com/uc?export=view&id=15gvSvOP_xwBJQgHixlf8z-7cGM7mapj8"
+    mediaType: "image",
+    mediaSrc: "cesped.jpg"
   }
 ];
 
@@ -165,6 +165,7 @@ modal.addEventListener('click', (e) => {
     }
   }
 });
+
 
 
 
